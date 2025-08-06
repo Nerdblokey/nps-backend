935fdb7a76bb9f6a1186d9c53d219e78b3444638
Nerdblokey <96788042+Nerdblokey@users.noreply.github.com>
2025-08-06 11:25:12 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,10 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  query: (text, params) => pool.query(text, params)
 };
-
